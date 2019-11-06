3766c33889716788ab5c53abfefdb8c567b5b1e3
github username <github email address>
2019-11-05 17:43:17 -0800

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,17 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+        name TEXT,
+        age INTEGER,
+        gender TEXT,
+        color TEXT,
+        temperament TEXT,
+        alive BOOLEAN
+);
+
+/*CREATE TABLE cats (
+  id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+);
+*/
+
